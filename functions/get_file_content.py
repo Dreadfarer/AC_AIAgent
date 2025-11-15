70d15a835a8734111dd082e6e2fa6c49f89008aa
Dreadfarer <niraiya@gmail.com>
2025-11-14 19:41:44 -0500

    debugging

---
+++ functions/get_file_content.py
@@ -0,0 +1,32 @@
+import os
+from config import *
+
+def get_file_content(working_directory, file_path):
+    """
+    Reads the content of a file located at the specified path within the given working directory.
+    Args:
+        working_directory (str): The base directory where the file is located.
+        file_path (str): The relative path to the file from the working directory.
+    Returns:
+        str: The content of the file as a string.
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        IOError: If there is an error reading the file.
+    """
+
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(target_file, 'r') as file:
+            content = file.read()
+            if len(content) > MAX_CHARS:
+                return content[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+            return content
+        
+    except Exception as e:
+        return f"Error reading file: {e}"
---
+++ tests.py
@@ -0,0 +1,13 @@
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
+def test():
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for current file:")
+    print(result)
+    print("")
+
+
+
+if __name__ == "__main__":
+    test()
